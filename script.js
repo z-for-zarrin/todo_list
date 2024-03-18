48d98a0a0a19f08aaf90e3695f792d20c9134d41
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-03-18 14:33:30 +0000

    can add items to todo list

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("#todo-form");
+const inputText = document.querySelector("#new-todo");
+const toDoList = document.querySelector("#list");
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    console.log(evt);
+    const newListItem = document.createElement("li");
+    newListItem.innerText = evt.target["new-todo"].value;
+    toDoList.appendChild(newListItem);
+});
